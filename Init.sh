f4d3108f7ab42781a376514cebc1330a507d7e2d
Alec Fuglem <afuglem@purdue.edu>
2019-05-30 09:20:55 -0400

    Commits almost fiully working bash init

--- Init.sh
+++ Init.sh
@@ -1,54 +1,5 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NOCOLOR='\033[0m'
-
-function InitScripts {
-
-  currentFullDirPath=$( cd "$(dirname "$0")" ; pwd -P )
-  outputLoaderFile="zzzz_Toasting.tst"
-
-  # This is where it gets all the files that are "Bash......." to import, done this way to onyl get current dir
-  bashSetupFiles=$(find $currentFullDirPath -type f -iname "Bash*.sh" -maxdepth 1)
-
-  # Tests if the loader file exist and creates it if it doesn't
-  test -e ${outputLoaderFile} || touch ${outputLoaderFile}
-
-  for currentFile in $bashSetupFiles; do # Not recommended, will break on whitespace
-
-    if grep -qxF 'if [ -f '$currentFile' ]; then' ${outputLoaderFile}; then
-
-      printf "${GREEN}Setup Complete for ${NOCOLOR}""$currentFile\n"
-    else
-      printf "${RED}Setting up ${NOCOLOR}""$currentFile\n"
-      echo 'if [ -f '$currentFile' ]; then' >> $outputLoaderFile
-      echo -e '\tsource '$currentFile >> $outputLoaderFile
-      echo 'fi' >> $outputLoaderFile
-
-    fi
-  done
-}
-
-InitScripts
-
-
-
-exit 1
-
-#Must be startedt eith <<. ./Init.sh>> from the command line!!! If not autoreload does not work
-
-
-
-
-################## All old Init code is below #################
-
-
-
-
-
-
-
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         echo "Set OS to Linux"
         bashProfileOrRc=~/.bashrc
@@ -60,65 +11,48 @@ else
         exit
 fi
 
-dividerString="!---!"
-collectiveBashFile=~/BashScripts/.bash_loader
-
-### Colors Setup
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 
 function InitScripts {
 
-  for filename in .bash_[^l]*; do
-    # Does this regex to ignore sourcing the collective file
+  currentFullDirPath=$( cd "$(dirname "$0")" ; pwd -P )
+  outputLoaderFile=~/BashScripts/zzzz_Toasting.tst
 
-    #First check the file has a valid "auto_add" feaute
-    if grep -q $dividerString "$filename"; then
+  # This is where it gets all the files that are "Bash......." to import, done this way to onyl get current dir
+  bashSetupFiles=$(find $currentFullDirPath -type f -iname "Bash*.sh" -maxdepth 1)
 
-      #Split the file on its comments since they are always commented
-      oldIFS="$IFS"
-      IFS=$'\n#' arr=($(<"$filename"))
-      IFS="$oldIFS"
+  # Tests if the loader file exist and creates it if it doesn't
+  test -e ${outputLoaderFile} || touch ${outputLoaderFile}
 
-      if [[ "${arr[4]}" =~ $dividerString ]]
-      then
+  for currentFile in $bashSetupFiles; do # Not recommended, will break on whitespace
 
-         #Make an array to store the concatinated string
-         mashOfCharactersToMatchInFile=""
+    # The following code inserts this example line for all scripts into the loader
+    # if [ -f /Users/afuglem/BashScripts/BashEceHw.sh ]; then
+    # 	source /Users/afuglem/BashScripts/BashEceHw.sh
+    # fi
 
-         #Compres the info needed into one string with no whitesapce
-         for i in {1..3}; do
-            currentLineNoWhiteSpace=$(echo "${arr[$i]}" | sed 's/ //g' | xargs)
-            printf -v mashOfCharactersToMatchInFile "%s%s" $mashOfCharactersToMatchInFile $currentLineNoWhiteSpace
-        done
+    if grep -qxF 'if [ -f '$currentFile' ]; then' ${outputLoaderFile}; then
 
-        #Gather entire bash profile into single string no whitespace
-        bashProfStandin=$(tr -d ' \t\n\r\f' <$collectiveBashFile)
+      printf "${GREEN}Setup Complete for ${NOCOLOR}""$currentFile\n"
+    else
+      printf "${RED}Setting up ${NOCOLOR}""$currentFile\n"
+      echo 'if [ -f '$currentFile' ]; then' >> ${outputLoaderFile}
+      echo -e '\tsource '$currentFile >> ${outputLoaderFile}
+      echo 'fi' >> ${outputLoaderFile}
 
-        #See if the text in question exists in the file
-        if [[ $bashProfStandin == *"$mashOfCharactersToMatchInFile"* ]]; then
-          printf "Setup complete: ${GREEN}$filename${NOCOLOR}\n"
-        else
-          printf "File Not Initilized - Adding ${RED}$filename${NOCOLOR}\n"
-
-          for i in {1..3}; do
-             echo "${arr[$i]}" >> $collectiveBashFile
-          done
-        fi
-      fi
     fi
   done
 
-  echo "source $collectiveBashFile" >> $bashProfileOrRc
+  # Now that the loader is ready, add to the .bash_profile/rc
 
-  . $bashProfileOrRc
+  echo 'source '$outputLoaderFile >> $bashProfileOrRc
+
   echo "Reloading..."
-
+  . $bashProfileOrRc
 
   # This is where the script then calls the git downloader
-
 
   echo "Downloading git repos..."
 
@@ -130,5 +64,7 @@ function InitScripts {
 }
 
 InitScripts
+
+exit 1
 
 #Must be startedt eith <<. ./Init.sh>> from the command line!!! If not autoreload does not work
